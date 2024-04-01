b6f3196d6e4bc5215e33c811265ca73b23ab5bbd
Ayush Patil <patil.sayush@gmail.com>
2024-04-01 12:33:52 +0530

    This is my first commit

---
+++ new.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"This is my first git and github repository that adds the files to github and also pulls the code from github";
+    return 0;
+}
