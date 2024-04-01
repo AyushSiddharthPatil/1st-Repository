868450759d7c18ce5d702989557a39af0026fe10
Ayush Patil <patil.sayush@gmail.com>
2024-04-01 13:12:42 +0530

    Added a new cout

--- new.cpp
+++ new.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main()
 {
     cout<<"This is my first git and github repository that adds the files to github and also pulls the code from github";
+    cout<<"I understood how git and github works fluently";
     return 0;
 }
