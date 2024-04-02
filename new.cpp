dcf53a92e21e352e13d0fb233cf4ec3d1e4d413d
Ayush Patil <patil.sayush@gmail.com>
2024-04-02 13:55:52 +0530

    This is my 2nd commit

--- new.cpp
+++ new.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main()
 {
-    cout<<"This is my first git and github repository that adds the files to github and also pulls the code from github";
+    cout<<"This is my first git and github repository that adds the files to github and also pulls the code from github"<<endl;
     cout<<"I understood how git and github works fluently";
     return 0;
 }
